dc662dd357ad4a7b65b059f65a44a40158294f47
Arabe2000 <arberecebimentos@gmail.com>
2025-06-26 00:13:33 -0300

    aaa

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou especifique seu domínio
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+connections = []
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    connections.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for conn in connections:
+                if conn != websocket:
+                    await conn.send_text(data)
+    except:
+        pass
+    finally:
+        connections.remove(websocket)
